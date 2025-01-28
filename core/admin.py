d72a6a73a4357aa359f0e1226cbf2a0baa1edf1c
Atebisun David <davidolaoluwa240@gmail.com>
2025-01-28 13:06:36 +0100

    Added custom user admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Exam, Session, SessionNote
 
+'''
+    Custom User Admin
+'''
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('avatar', 'email_frequency')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('avatar', 'email_frequency')}),
+    )
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Exam)
 admin.site.register(Session)
 admin.site.register(SessionNote)
